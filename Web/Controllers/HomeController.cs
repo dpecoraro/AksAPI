1f482a05d9c3a88691665f9c724dd89425f25d99
Demetrius Pecoraro <demetrius.pecoraro@totvs.com.br>
2021-04-06 22:38:50 -0300

    Initial commit

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+
+                var httpClient = new HttpClient();
+
+                var req = await httpClient.GetAsync("http://aksapi/WeatherForecast");
+
+                if (req.IsSuccessStatusCode)
+                {
+                    var response = await req.Content.ReadAsStringAsync();
+
+                    var forecast = JsonConvert.DeserializeObject<List<WeatherForecast>>(response);
+
+                    var viewModel = new WeatherForecastViewModel { Forecasts = forecast };
+
+                    return View(viewModel);
+                }
+
+                return View(new WeatherForecastViewModel { Forecasts = new List<WeatherForecast>() });
+            }
+            catch (Exception ex)
+            {
+                return View(new WeatherForecastViewModel { Forecasts = new List<WeatherForecast>() });
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Web/Models/WeatherForecastViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class WeatherForecastViewModel
+    {
+        public WeatherForecast Forecast { get; set; }
+
+        public List<WeatherForecast> Forecasts { get; set; }
+    }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
